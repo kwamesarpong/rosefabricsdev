8986a454bd869ca40c4b6e494c18d42b953d0151
Kwame Sarpong <kayosarp@gmail.com>
2018-05-04 12:36:02 +0100

    Search created

--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,22 @@
 import React, { PureComponent } from 'react'
-import { View, StyleSheet } from 'react-native'
-import { SearchBar } from 'react-native-elements';
+import { View, StyleSheet, TouchableOpacity, Text, Alert } from 'react-native'
 
 
 class Search extends PureComponent {
+
+    handleSearch = () => {
+        Alert.alert("Howdy")
+    }
+
     render(){
         return (
-            <SearchBar
-             lightTheme
-             containerStyle={{backgroundColor: 'white'}}
-             placeholder='Search Here...'
-             />
+            <View style={{flex: 1, flexDirection: 'row'}}>
+                <TouchableOpacity style={{width: '100%', paddingTop: 5, paddingBottom: 5, paddingLeft: 5, paddingRight: 5}} onPress={this.handleSearch}>
+                    <View style={{width: '100%', paddingTop: 10, paddingBottom: 10, alignItems: 'center', backgroundColor: '#f4f4f4'}}>
+                        <Text>Tap here to search ... </Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
         )
     }
 }
